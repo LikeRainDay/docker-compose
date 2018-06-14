a03bef856bd76468f92291a407cee99dcd7520f3
houshuai <houshuai>
2018-06-14 16:30:20 +0800

    进行更改脚本

--- docker_onlion/zookpeer-slaver/test_compose.sh
+++ docker_onlion/zookpeer-slaver/test_compose.sh
@@ -8,5 +8,5 @@ docker run -it --rm \
          --link zoo1:zk1 \
          --link zoo2:zk2 \
          --link zoo3:zk3 \
-         --net zktest_default \
+         --net zk_test_front-tier \
          zookeeper zkCli.sh -server zk1:2181,zk2:2181,zk3:2181
