411c58fae591550324a9ec2f8facff36a7271b55
houshuai <houshuai>
2018-06-14 15:12:00 +0800

    构建离线的nginx

---
+++ docker_manage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+# 删除已停止的容器
+sudo docker ps -q -a | xargs sudo docker rm
+# 删除无效的镜像
+sudo docker rmi $(sudo docker images | grep "^" | awk '{print $3}')
---
+++ docker_offline/dockerfile/nginx/config/run-step.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# 进行运行当前nginx的脚本
+/usr/sbin/sshd &
+/usr/local/nginx/sbin/nginx
