1d9c39fae9f88b05160cd33d08ab315352099546
houshuai <houshuai>
2018-06-14 15:40:08 +0800

    增加删除 构建时候出现的无用<none> 的镜像

--- docker_manage.sh
+++ docker_manage.sh
@@ -4,3 +4,6 @@
 sudo docker ps -q -a | xargs sudo docker rm
 # 删除无效的镜像
 sudo docker rmi $(sudo docker images | grep "^" | awk '{print $3}')
+
+
+sudo docker rmi $(sudo docker images | grep "<none>" | awk '{print $3}')
