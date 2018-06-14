cdaa46f911f41f3835f96f58f9ee9ad5942079a2
houshuai <houshuai>
2018-06-14 15:40:57 +0800

    更改sql

---
+++ docker_offline/dockerfile/mysql/config/scheme.sql
@@ -0,0 +1,7 @@
+-- 进行创建对应的表，并使用当前创建的表 进行填充数据
+drop database if exists  `model_market`;
+
+create database IF NOT EXISTS `model_market` default character set utf8 collate utf8_general_ci;	
+
+-- flush privileges; 
+
