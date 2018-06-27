5dd8286ef41aa4a6c1c7604ce6a905ce55e743b8
18f_100000605004_houshuai0816 <18f_100000605004_houshuai0816@git.cloud.tencent.com>
2018-06-27 21:59:52 +0800

    增加Spark的脚本文件

--- docker_onlion/rabbitmq/start-rabbitmq.sh
+++ docker_onlion/rabbitmq/start-rabbitmq.sh
@@ -2,4 +2,4 @@
 
 # 进行开启Rabbit 功能
 
-docker run -d --hostname rabbit --name rabbit -e RABBITMQ_DEFAULT_USER=rabbitadmin -e RABBITMQ_DEFAULT_PASS=rabbitpwd -p 15672:15672 rabbitmq:3-management
+docker run -d --hostname rabbit --name txt-rabbitmq -e RABBITMQ_DEFAULT_USER=houshuai -e RABBITMQ_DEFAULT_PASS=houshuai -p 15672:15672 rabbitmq:3-management
