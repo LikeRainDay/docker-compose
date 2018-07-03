c28de78de96a1a30308195be9fadb30b3dc6c9c6
18f_100000605004_houshuai0816 <18f_100000605004_houshuai0816@git.cloud.tencent.com>
2018-07-03 23:57:22 +0800

    更改Dockerfile脚本 由于固定IP需要有容器。Container hadoop-master not found, and Docker not installed.

--- docker_onlion/hadoop/base/shell/create-net.sh
+++ docker_onlion/hadoop/base/shell/create-net.sh
@@ -8,7 +8,7 @@ ip link set dev br0 up
 ip addr add 192.168.2.1/24 dev br0
 
 echo "给容器设置固定ip"
-pipework br0 hadoop-master 192.168.2.10/24
-pipework br0 hadoop-slave1 192.168.2.11/24
-pipework br0 hadoop-slave2 192.168.2.12/24
-pipework br0 hadoop-slave3 192.168.2.13/24
+/usr/local/bin/pipework br0 hadoop-master 192.168.2.10/24
+/usr/local/bin/pipework br0 hadoop-slave1 192.168.2.11/24
+/usr/local/bin/pipework br0 hadoop-slave2 192.168.2.12/24
+/usr/local/bin/pipework br0 hadoop-slave3 192.168.2.13/24
