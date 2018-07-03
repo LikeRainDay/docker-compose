03356e89cd541661654951090e8686db5207e144
18f_100000605004_houshuai0816 <18f_100000605004_houshuai0816@git.cloud.tencent.com>
2018-07-03 22:04:48 +0800

    增加编排工具 和 固定容器对应的IP

---
+++ docker_onlion/hadoop/base/shell/create-net.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 进行创建对应的内容
+
+echo "创建网络"
+brctl addbr br0
+ip link set dev br0 up
+ip addr add 192.168.2.1/24 dev br0
+
+echo "给容器设置固定ip"
+pipework br0 hadoop-master 192.168.2.10/24
+pipework br0 hadoop-slave1 192.168.2.11/24
+pipework br0 hadoop-slave2 192.168.2.12/24
+pipework br0 hadoop-slave3 192.168.2.13/24
