f7316e82ee5214c8524304e7066bf1c9d0876681
houshuai <houshuai>
2018-07-04 10:25:41 +0800

    更改创建网桥的方式，进行固定Docker的网桥。（在宿主主机上进行固定。）

--- docker_onlion/hadoop/create-net.sh
+++ docker_onlion/hadoop/create-net.sh
@@ -2,7 +2,21 @@
 
 # 进行创建对应的内容
 
+echo "准备安装环境"
+brew install wget bridget-utils -y
+
+
+brctl show
+
+echo "进行安装"
+wget https://github.com/jpetazzo/pipework/archive/master.zip \
+    && unzip master.zip \
+    && mv pipework-master pipework \
+    && cp -rp pipework/pipework /usr/local/bin/pipework \
+    && rm -rf pipework-master.zip
+
 echo "创建网络"
+
 brctl addbr br0
 ip link set dev br0 up
 ip addr add 192.168.2.1/24 dev br0
